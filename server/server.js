3144712ec97b038066d6662420a4b17d16bb9bf8
Wagner D'Amaral <32019817+wdamaral@users.noreply.github.com>
2018-08-12 12:58:48 -0400

    Update server.js to use heroku

--- server/server.js
+++ server/server.js
@@ -14,27 +14,25 @@ const PORT = process.env.PORT;
 const URL = process.env.URL;
 
 const bot = new Telegraf(BOT_TOKEN);
+bot.catch((err) => {
+  console.log('Sorry! Not able to connect to Telegram.');
+  return
+});
 
 // app.use(bodyParser.json());
 
 if(process.env.NODE_ENV === 'production') {
 bot.telegram.setWebhook(`${URL}/bot${BOT_TOKEN}`);
 app.use(bot.webhookCallback(`/bot${BOT_TOKEN}`));
-/*
- your bot commands and all the other stuff on here ....
-*/
-bot.start((ctx) => ctx.reply('Welcome'));
-bot.hears('hi', (ctx) => ctx.reply('Hey there' + ctx.from.id));
-// and at the end just start server on PORT
 } else {
-    bot.start((ctx) => ctx.reply('Welcome'));
-    bot.hears('hi', (ctx) => ctx.reply('Hey there' + ctx.from.id));
     bot.startPolling();
 }
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
+bot.start((ctx) => ctx.reply('Welcome'));
+bot.hears('hi', (ctx) => ctx.reply('Hey there' + ctx.from.id));
 //app.post()
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
